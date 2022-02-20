c6a495fe2dcce30ec948d345e5eae57a5f8e8098
이강준 <47883234+Ladun@users.noreply.github.com>
2022-02-20 21:56:03 +0900

    Example making error fix

--- custom_dataset/nli_dataset.py
+++ custom_dataset/nli_dataset.py
@@ -38,7 +38,7 @@ class CustomNLIProcessor(DataProcessor):
             guid = "%s-%s" % (set_type, i)
             text_a = line['premise']
             text_b = line['hypothesis']
-            label = str(line['label']) if set_type != 'test' else None
+            label = line['label'] if set_type != 'test' else None
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
